2c382bed3073b850869085a511ca9c4d0bbc8cc4
KartheekS786 <49051788+KartheekS786@users.noreply.github.com>
2020-07-04 16:46:49 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template("index1.html")
+
+@app.route('/index2/')
+def index2():
+  return render_template("index2.html")
+  
+if __name__ == "__main__":
+  app.run(debug=True)
